31d7b429502b488075fb5a6fbe4348d85b7b4887
mams95190 <51932567+mams95190@users.noreply.github.com>
2026-07-03 23:58:35 +0000

    hardcode compile sdk and ndk for android build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,13 +6,13 @@ plugins {
 
 android {
     namespace = "com.example.peugeot205_dashboard"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 36
+    ndkVersion = "27.0.12077973"
 
     defaultConfig {
         applicationId = "com.example.peugeot205_dashboard"
         minSdk = 21
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }

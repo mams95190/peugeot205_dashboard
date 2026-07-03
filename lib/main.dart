8c4de609e95737e831f2795079240bd86fd34f46
mams95190 <51932567+mams95190@users.noreply.github.com>
2026-07-03 23:51:50 +0000

    add required flutter blue plus license parameter

--- lib/main.dart
+++ lib/main.dart
@@ -201,6 +201,7 @@ class _HomePageState extends State<HomePage> {
       });
 
       await device.connect(
+        license: License.nonprofit,
         timeout: const Duration(seconds: 10),
         autoConnect: false,
       );
